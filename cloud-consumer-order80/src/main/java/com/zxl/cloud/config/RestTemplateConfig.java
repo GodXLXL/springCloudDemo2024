769b87b616afd946793145a0803baf9323434d29
GodXLXL <1840200639@qq.com>
2024-03-29 00:01:51 +0800

    springCloud2024 demo by GodXLXL

--- cloud-consumer-order80/src/main/java/com/zxl/cloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80/src/main/java/com/zxl/cloud/config/RestTemplateConfig.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
 import org.springframework.web.client.RestTemplate;
 
 /**
